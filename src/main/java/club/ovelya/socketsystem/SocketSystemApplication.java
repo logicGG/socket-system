eaf5afded960b6073749d7b4edb4935f570c6207
黄宏基 <huanghj@gditsec.org.cn>
2023-08-11 10:21:01 +0800

    init

---
+++ src/main/java/club/ovelya/socketsystem/SocketSystemApplication.java
@@ -0,0 +1,13 @@
+package club.ovelya.socketsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocketSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SocketSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/club/ovelya/socketsystem/SocketSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package club.ovelya.socketsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocketSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
