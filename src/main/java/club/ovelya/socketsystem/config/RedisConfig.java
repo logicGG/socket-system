cadaa5d2e2c079579439b808d61dd9dcafcceb80
黄宏基 <huanghj@gditsec.org.cn>
2023-08-24 10:49:49 +0800

    jedis配置

--- src/main/java/club/ovelya/socketsystem/config/RedisConfig.java
+++ src/main/java/club/ovelya/socketsystem/config/RedisConfig.java
@@ -1,8 +1,10 @@
 package club.ovelya.socketsystem.config;
 
+import java.time.Duration;
 import org.springframework.cache.annotation.CachingConfigurer;
 import org.springframework.cache.interceptor.KeyGenerator;
 import org.springframework.context.annotation.Bean;
+import redis.clients.jedis.JedisPoolConfig;
 
 public class RedisConfig implements CachingConfigurer {
 
@@ -17,5 +19,17 @@ public class RedisConfig implements CachingConfigurer {
             }
             return sb.toString();
         };
+    }
+
+    @Bean
+    public JedisPoolConfig jedisPoolConfig() {
+        JedisPoolConfig jedisPoolConfig = new JedisPoolConfig();
+        jedisPoolConfig.setBlockWhenExhausted(true);
+        jedisPoolConfig.setTestOnBorrow(true);
+        jedisPoolConfig.setMaxTotal(100);
+        jedisPoolConfig.setMaxIdle(20);
+        jedisPoolConfig.setMinIdle(5);
+        jedisPoolConfig.setMaxWait(Duration.ofMillis(5000));
+        return jedisPoolConfig;
     }
 }
